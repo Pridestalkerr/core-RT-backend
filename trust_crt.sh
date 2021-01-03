efbaacd02f614fe93f0d5748e011f4505187266a
dan <danartenie158@gmail.com>
2021-01-03 07:42:02 +0000

    Platform independent localhost ssl cert creation. Use "dotnet dev-certs" if available.

---
+++ trust_crt.sh
@@ -0,0 +1,3 @@
+sudo trust anchor ca.crt
+export ASPNETCORE_Kestrel__Certificates__Default__Password="000"
+export ASPNETCORE_Kestrel__Certificates__Default__Path="/home/pridestalkerr/Documents/apps/core-RT-backend/localhost.pfx"
